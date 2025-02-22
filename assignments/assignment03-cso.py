f08caeca1c5d2a2a14a6fdeead01693fa8cbd0a0
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-02-22 08:59:22 +0000

    assignment3-cso

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,15 @@
+# Write a program that retrieves the dataset for the "Exchequer Account (Historical Series)" from data.cso.ie
+# Store it into a file called "cso.json".
+# Author: Irene Kilgannon
+
+import requests
+import json
+
+url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en'
+
+response = requests.get(url)
+data = response.json()
+
+# Save to cso.json
+with open("data/cso.json", "w") as f:    
+    json.dump(data, f)
