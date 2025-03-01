75f19d8747159a27c415e27ce6745fdc60080c03
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-03-01 07:46:43 +0000

    finished lab5

--- labs/Week05-Authentication/lab5.03-githubbymodule.py
+++ labs/Week05-Authentication/lab5.03-githubbymodule.py
@@ -20,7 +20,7 @@ repo = g.get_repo("IreneKilgannon/WSAA-coursework")
 #   print(content.path)
 
 ########
-# To view the contents of the labs directory in the repository
+# To view the contents of the labs directory
 folder_path = "labs"
 
 contents = repo.get_contents(folder_path)
@@ -29,27 +29,31 @@ for file in contents:
 
 #######
 # To view the contents of a file inside a directory
-file_path = "labs/test.txt"
+file_path = "labs/text.txt"
 
-file_path = repo.get_contents()
 file_info = repo.get_contents(file_path)
 
 print(f"File Name: {file_info.name}")
 print(f"Download URL: {file_info.download_url}")
 
+#######
+# To upload a file to a specific directory
+#file_path = "labs/new_file.txt"
+#repo.create_file(file_path, "Add new file", "Hello, GitHub!", branch="main")
+
 # Download the url of the file
-fileInfo = repo.get_contents("test.txt")
-urloffile = fileInfo.download_url
-#print(urloffile)
-
-# Use download URL to make a http request tot he file
-response = requests.get(urloffile)
-contentsOfFile = response.text
-#print(contentsOfFile)
-
-newContents = contentsOfFile + "more stuff \n"
-#print(newContents)
-
-# update the content of the file on github
-gitHubResponse = repo.update_file(fileInfo.path, "updated by prog", newContents, fileInfo.sha)
-print(gitHubResponse)
+#fileInfo = repo.get_contents("test.txt")
+#urloffile = fileInfo.download_url
+##print(urloffile)
+#
+## Use download URL to make a http request tot he file
+#response = requests.get(urloffile)
+#contentsOfFile = response.text
+##print(contentsOfFile)
+#
+#newContents = contentsOfFile + "more stuff \n"
+##print(newContents)
+#
+## update the content of the file on github
+#gitHubResponse = repo.update_file(fileInfo.path, "updated by prog", newContents, fileInfo.sha)
+#print(gitHubResponse)
