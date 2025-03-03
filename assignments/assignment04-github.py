e8af7b1ffaade5a8cb81e6ea89cec226ff19f097
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-03-03 09:37:40 +0000

    assignment04 task completed

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -11,24 +11,17 @@ from config import apikeys as cfg
 g = Github(cfg['githubkey'])
 
 
-repo = g.get_repo("IreneKilgannon/WSAA-coursework")
+repo = g.get_repo("IreneKilgannon/wsaa-assignment4")
 print(repo.clone_url)
 
-#To view the contents of the repository
+# View the contents of the repository
 contents = repo.get_contents("")
 for content in contents:
    print(content.path)
 
-# To view the contents of the assignments directory
-folder_path = "assignments"
-
-contents = repo.get_contents(folder_path)
-for file in contents:
-   print(file.path)
-
 #######
-# To view the contents of a file inside a directory
-file_path = "test.txt"
+# View the contents of a file inside a directory
+file_path = "text.txt"
 
 file_info = repo.get_contents(file_path)
 
@@ -45,26 +38,11 @@ print(url_of_file)
 response = requests.get(url_of_file)
 contentsOfFile = response.text
 print(contentsOfFile)
-#
 
-newContents = contentsOfFile.replace("Andrew", "Irene") + "more stuff \n"
-print(newContents)
-#
-## update the content of the file on github
-#gitHubResponse = repo.update_file(fileInfo.path, "updated by prog", newContents, fileInfo.sha)
-#print(gitHubResponse)
-# Starting basic, write a script to read a file and replace "Andrew" with my name
-# https://www.geeksforgeeks.org/how-to-search-and-replace-text-in-a-file-in-python/
-#
-#search_text = "Andrew"
-#
-#replace_text = "Irene"
-#
-#with open ('andrew.txt', "r") as f:
-#    data = f.read()
-#    data = data.replace(search_text, replace_text)
-#
-## Write the changes to the file. 
-#with open('andrew.txt', 'w') as f:
-#    f.write(data)
-#
+# Replace Andrew with Irene
+new_contents = contentsOfFile.replace("Andrew", "Irene")
+#print(new_contents)
+
+# Update the content of the file on Github
+gitHubResponse = repo.update_file(file_info.path, "updated by prog", new_contents, file_info.sha)
+print(gitHubResponse)
