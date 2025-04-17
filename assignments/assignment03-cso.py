e595262d97b4d19e1416f946357a17b98722a18e
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-04-17 13:01:57 +0100

    minor edits to assignment03-cso

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -5,12 +5,13 @@
 import requests
 import json
 
+# URL to the dataset from cso.ie.
 url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en'
 
-# Make a HTTP GET request
+# Make a HTTP GET request and convert it into JSON. 
 response = requests.get(url)
 data = response.json()
 
-# Save to cso.json
+# Save to data/cso.json
 with open("data/cso.json", "w") as f:    
     json.dump(data, f)
